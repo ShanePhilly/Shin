588edfa37ca9afca9fe49a331cd5809fa989dda5
Shane Phillips <shanephillips@Shanes-MacBook-Pro.local>
2021-03-14 23:04:44 -0400

    tutorials 2&3

--- server/index.js
+++ server/index.js
@@ -1,33 +1,49 @@
 const express = require('express');
 const ejs = require ('ejs');
 const path = require('path');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+
+// Navigation
 
 const clientPath = path.join(__dirname,'../client/')
 const staticPath = path.join(clientPath,'/static/');
 const viewsPath = path.join(clientPath,'/views/')
 
+//Basic server
+
 const app = express();
+app.use(express.static(staticPath));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({
+        name: 'sounds',
+        secret: 'infinitesounds',
+        saveUninitialized: false,
+        resave: false,
+        cookie: {
+            maxAge: 1000*60*60*24*3,
+        }
+}));
+
+app.listen(2000);
+
+// Setting views
 
 app.set('view engine','ejs');
 app.set('views',viewsPath);
 
-var x = 0;
-
-const counter = function(req, res, next) {
-    x++;
-    console.log(x);
-    next();
-}
-
-
-app.use(express.static(staticPath));
+// Routes
 
 app.get('/', function(req, res) {
-    res.render('index');
+    res.render('index', {data: req.session});
 });
 
-app.get('/index2', counter, function(req, res) {
-    res.render('Index2',{count: x});
+app.get('/index2', function(req, res) {
+    res.render('Index2', {data: req.session});
 });
 
-app.listen(2000);
+app.post('/welcome', (req, res) => {
+    console.log(req.body);
+    req.session.username=req.body.nombre;
+    res.send('SUCCESS');
+});
